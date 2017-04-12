7514de07b1a33461631dda5575ee7c00e66a3b30
Phong Le <phong.le@wgu.edu>
2017-04-12 15:41:50 -0600

    Initial

---
+++ TickerView/TickerView.h
@@ -0,0 +1,19 @@
+//
+//  TickerView.h
+//  TickerView
+//
+//  Created by Phong Le on 4/12/17.
+//  Copyright © 2017 Phong Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TickerView.
+FOUNDATION_EXPORT double TickerViewVersionNumber;
+
+//! Project version string for TickerView.
+FOUNDATION_EXPORT const unsigned char TickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TickerView/PublicHeader.h>
+
+#import "SBTickerView.h"
